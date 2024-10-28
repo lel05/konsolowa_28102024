6a37255137c28052a1abcb18bbd654086358d2dd
lel05 <graczkuba2005@gmail.com>
2024-10-28 12:19:45 +0100

    Utworzenie klasy Song

---
+++ Song.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace konsolowa
+{
+    public class Song
+    {
+        public string Artist { get; set; }
+        public string Album { get; set; }
+        public int SongsNumber { get; set; }
+        public int Year { get; set; }
+        public uint DownloadNumber { get; set; }
+    }
+}
